a22fa7ce452d4bbf30b0f7dca332ceae3581790c
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-11-13 09:22:52 -0300

    feat: initializing exercise 02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gde-jesu <gde-jesu@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 16:59:38 by gde-jesu          #+#    #+#             */
-/*   Updated: 2023/11/13 09:11:38 by gde-jesu         ###   ########.fr       */
+/*   Created: 2023/11/13 09:20:17 by gde-jesu          #+#    #+#             */
+/*   Updated: 2023/11/13 09:20:33 by gde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Zombie_HPP
-# define Zombie_HPP
-
-# include <iostream>
-
-class	Zombie {
-	private:
-		std::string _name;
-  
-	public:
-		Zombie();
-		~Zombie(void);
-		
-		std::string	getName();
-		void		setName(std::string name);
-		void		announce(void);
-};
-
-Zombie* zombieHorde( int N, std::string name );
-
-#endif
+int main()
+{
+    return (0);
+}
