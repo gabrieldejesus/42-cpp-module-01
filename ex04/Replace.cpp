7080b688788bd820a019da93a0f7c4b5a3c4e695
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2024-08-09 23:50:51 -0300

    feat: Update Replace class implementation and Makefile

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -6,7 +6,7 @@
 /*   By: gde-jesu <gde-jesu@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:07:36 by gde-jesu          #+#    #+#             */
-/*   Updated: 2024/05/23 08:23:32 by gde-jesu         ###   ########.fr       */
+/*   Updated: 2024/08/09 23:50:13 by gde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ Replace::Replace(std::string filename, std::string s1, std::string s2)
   
   std::cout << "👷 Constructor " << getFilename() << " called!" << std::endl;
 
-	std::string	line;
-  std::ifstream	originalFile;
-	std::ofstream	replaceFile;
+	std::string   line;
+  std::ifstream originalFile;
+	std::ofstream replaceFile;
 	
 	originalFile.open(getFilename().c_str());
 	replaceFile.open(getFilename().append(".replace").c_str());
-
+  
 	while (getline(originalFile, line))
 	{
 		std::size_t	found = 0;
 		std::string newLine = line;
-
+  
 		while ((found = newLine.find(this->_s1, found)) != std::string::npos) {
 			newLine.erase(found, this->_s1.length());
 			newLine.insert(found, this->_s2);
